0b1bcb2c829431b3825698066e48408ad4a8b04d
AyushSharma-17 <ayushsharma12345qwerty@gmail.com>
2023-12-03 01:30:05 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main {
+     public static void main(String[] args)
+{
+    System.out.println("this is java project from jenkins");
+    System.out.println(new Date());
+
+    
+}
+    } 
+    
